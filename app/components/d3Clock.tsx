f6b09be65a633e02655c440d0f656e989b206fe8
tzh <3136374549@qq.com>
2024-07-09 23:49:34 +0800

    add draggable clock

--- app/components/d3Clock.tsx
+++ app/components/d3Clock.tsx
@@ -4,11 +4,24 @@ import * as d3 from 'd3';
 export default function D3Clock(
   clockSize: number = 300,
 ) {
+  const radius = clockSize / 2;
+  const centerX = radius;
+  const centerY = radius;
+
   useEffect(() => {
     const secondsHand = d3.select<SVGLineElement, {}>('#secondsHand');
   
-    function dragged(this: SVGLineElement, d: any) {
+    function dragged(this: SVGLineElement, event: DragEvent) {
       console.log('dragged');
+      const dx = event.x - centerX
+      const dy = event.y - centerY
+      const angle = Math.atan2(dy, dx)
+      const newX = centerX + radius * Math.cos(angle)
+      const newY = centerY + radius * Math.sin(angle)
+
+      d3.select(this)
+        .attr('x2', newX)
+        .attr('y2', newY)
     }
   
     const DragBehavior = d3.drag<SVGLineElement, any>()
