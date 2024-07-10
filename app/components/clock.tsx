b33497b8c5f83c39ccfaadf00d7d6691e38a38d0
tzh <3136374549@qq.com>
2024-07-10 16:46:24 +0800

    Add a mapping from the degree hand to current second num.

--- app/components/clock.tsx
+++ app/components/clock.tsx
@@ -3,6 +3,7 @@ import Pivot from './pivot';
 import Minute from './minute';
 import ScaleGroup from './scale';
 import Hour from './hour';
+import { useState } from 'react';
 
 export default function Clock(
   clockContainerSize: number = 400,
@@ -12,16 +13,22 @@ export default function Clock(
   const radius = clockSize / 2;
   const centerX = clockContainerSize / 2;
   const centerY = clockContainerSize / 2;
+  const [second, setSecond] = useState<number>(0);
+
+  const date = new Date();
 
   return (
+    <div>
     <svg style={{width: clockContainerSize, height: clockContainerSize}}>
       <circle cx={centerX} cy={centerY} r={radius} stroke='black' strokeWidth={2} fill='transparent'></circle>
       {ScaleGroup(radius, centerX, centerY)}
-      {Second(radius, centerX, centerY)}
+      {Second(radius, centerX, centerY, date.getSeconds(), (newSecond) => setSecond(newSecond))}
       {Minute(radius, centerX, centerY)}
       {Hour(radius, centerX, centerY)}
       {/* 表盘中心的小圆圈 */}
       {Pivot(radius, centerX, centerY)}
     </svg>
+    <div>{second}</div>
+    </div>
   );
 }
--- app/components/newSecond.tsx
+++ app/components/newSecond.tsx
@@ -5,33 +5,31 @@ import * as d3 from 'd3';
 export default function Second(
   radius: number,
   centerX: number, centerY: number,
-  initSecond: number,
+  prevSecond: number, currentSecond: number,
   updateSecond: (newSecond: number) => void
 ) {
   const handWidth = radius * 0.015;
   const handHeight = radius * 0.9;
   const handColor = 'red'
 
-  // 初始化秒针
+  // 初始化秒针到当前时间
   function InitialSecondHand() {
-    const secStart = `${(initSecond / 60) * 360}deg`;
-    const secEnd = `${(initSecond / 60) * 360 + 360}deg`;
+    const sec = new Date().getSeconds();
+    const secStart = `${(sec / 60) * 360}deg`;
+    const secEnd = `${(sec / 60) * 360 + 6}deg`;
     document.documentElement.style.setProperty('--sec-rot-start', secStart);
     document.documentElement.style.setProperty('--sec-rot-end', secEnd);
   };
+
   useEffect(() => {
     InitialSecondHand();
   }, []);
 
+
   // 实现拖动功能
 
-  // 当前指针是否停止移动
-  // 拖拽指针时，停止指针的移动
-  const [stopMoving, setStop] = useState(false);
+  const [dragging, setDragging] = useState(false);
 
-  // 响应拖拽事件，根据拖拽的位置设置新的角度
-  // 设置 x2 y2，让指针跟随鼠标移动
-  // 设置 css 变量，让指针在拖拽结束后从原位置开始移动
   function setNewAngle(elem: SVGLineElement, event: DragEvent) {
     const dx = event.x - centerX
     const dy = event.y - centerY
@@ -49,17 +47,19 @@ export default function Second(
     document.documentElement.style.setProperty('--sec-rot-end', endDeg);
   }
   
-  // 在 start 和 end 事件中都设置指针的位置，是为了防止拖拽开始和结束时刻产生抖动现象。
+  // 在 start 和 end 事件中都设置指针的位置，是为了防止抖动现象。
   function startDragging(this: SVGLineElement, event: DragEvent) {
-    setStop(true);
+    setDragging(true);
     setNewAngle(this, event);
   }
+
   function dragged(this: SVGLineElement, event: DragEvent) {
     setNewAngle(this, event);
   }
+
   function endDragging(this: SVGLineElement, event: DragEvent) {
     setNewAngle(this, event);
-    setStop(false);
+    setDragging(false);
   }
 
   const DragBehavior = d3.drag<SVGLineElement, any>()
@@ -72,32 +72,40 @@ export default function Second(
     secondHand.call(DragBehavior);
   }, [])
 
-  // 根据角度实时更新秒数
+  // 每秒获取秒针当前的角度，根据角度更新秒数
   useEffect(() => {
     const interval = setInterval(() => {
-      // 如果指针停止移动，不更新秒数
-      if (stopMoving) return;
+      // const secondHand = document.getElementById('secondHand');
+      // if (!secondHand) return;
+      // const x2 = parseFloat(secondHand.getAttribute('x2') || '0');
+      // const y2 = parseFloat(secondHand.getAttribute('y2') || '0');
+      // const dx = x2 - centerX;
+      // const dy = y2 - centerY;
+      // const angle = Math.atan2(dy, dx);
+      // const newSecond = Math.round(angle / (2 * Math.PI) * 60);
+      // updateSecond(newSecond);
 
-      // 获取旋转角度
-      const secondHand = document.getElementById('secondHand'); if (!secondHand) return;
+      if (dragging) return;
+      const secondHand = document.getElementById('secondHand');
+      if (!secondHand) return;
       const transform = window.getComputedStyle(secondHand).transform;
-      // 转移矩阵的格式为 "matrix(cos, sin, -sin, cos, 0, 0)"
-      var values = transform.split('(')[1].split(')')[0].split(',');
+      if (!transform) return;
+      var values
+      try {
+        values = transform.split('(')[1].split(')')[0].split(',');
+      }
+      catch(e) {
+        return;
+      }
       const a = parseFloat(values[0]);
       const b = parseFloat(values[1]);
-      let angle = Math.atan2(b, a) * (180 / Math.PI);
-      // 根据 x2 y2 修正角度（x2, y2 在拖拽指针后会发生变化）
-      const x2 = parseFloat(secondHand.getAttribute('x2') || '0');
-      const y2 = parseFloat(secondHand.getAttribute('y2') || '0');
-      const dx = x2 - centerX;
-      const dy = centerY - y2;
-      angle += Math.atan2(dx, dy) * (180 / Math.PI);
+      let angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
       if (angle < 0) angle += 360;
       const newSecond = Math.round(angle / 360 * 60);
       updateSecond(newSecond);
     }, 500);
     return () => clearInterval(interval);
-  }, [stopMoving]);
+  }, []);
 
   return (<line
     id='secondHand'
@@ -105,7 +113,7 @@ export default function Second(
     y1={centerY}
     x2={centerX}
     y2={centerY - handHeight}
-    className= { stopMoving ? '' : `${styles.secondHandAnimation}`}
+    className= { dragging ? '' : `${styles.secondHandAnimation}`}
     style={{stroke: handColor, strokeWidth: handWidth, transformOrigin: `${centerX}px ${centerY}px`}}
   ></line>)
 }
